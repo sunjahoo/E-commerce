69367ad44d13951b33aa9762eaec31ecfeebca45
sunjahoo <sunjahoo@naver.com>
2022-11-23 16:04:56 +1100

    update detail product page

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import {map} from 'rxjs/operators'
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +43,9 @@ export class ShopService {
       );
   }
 
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
   }
