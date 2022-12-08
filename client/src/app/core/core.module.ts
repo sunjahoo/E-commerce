6a3f623e3110735e3a4b06094eb56065570bfad6
sunjahoo <sunjahoo@naver.com>
2022-12-08 16:49:44 +1100

    add breadcrumb

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,15 +5,18 @@ import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
-
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+    SectionHeaderComponent,
   ],
-  exports: [NavBarComponent]
+  imports: [CommonModule, RouterModule, BreadcrumbModule],
+  exports: [NavBarComponent, SectionHeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
